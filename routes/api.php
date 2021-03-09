7b1a0e99b40fdae36f9644a82cbe82d5b7de8088
Levin LUN <chocolinax@gmail.com>
2021-03-09 16:35:16 +0800

    implement system and module relationship

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\Device;
+use App\Models\Module;
+use App\Models\System;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Validator;
@@ -37,6 +39,26 @@ Route::post('/tokens/create', function (Request $request) {
         $token = $user->createToken($request->device_id);
 
         $response = ['token' => $token->plainTextToken];
+    }
+
+    return $response;
+});
+
+Route::post('/module/create', function (Request $request) {
+
+    $validator = Validator::make($request->all(), [
+        'name' => 'required',
+    ]);
+
+    if ($validator->fails()) {
+        $response = $validator->messages();
+    } else {
+        $sys =  System::firstOrCreate();
+
+        $response = Module::create([
+            'name' => $request->name,
+            'system_id' => $sys->id()
+        ]);
     }
 
     return $response;
