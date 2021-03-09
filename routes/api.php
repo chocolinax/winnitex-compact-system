61fc4875b62033b3ab473986f00a01fce163f644
Levin LUN <chocolinax@gmail.com>
2021-03-09 17:20:34 +0800

    no message

--- routes/api.php
+++ routes/api.php
@@ -53,11 +53,11 @@ Route::post('/module/create', function (Request $request) {
     if ($validator->fails()) {
         $response = $validator->messages();
     } else {
-        $sys = System::firstOrCreate(['id' => 1]);
+        $s = System::firstOrCreate();
 
         $response = Module::create([
             'name' => $request->name,
-            'system_id' => $sys->getKey()
+            'system_id' => $s->getKey()
         ]);
     }
 
