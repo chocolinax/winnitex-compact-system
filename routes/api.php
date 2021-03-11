62305c0860c0e8eb1ff0e4c59ca2deb27aebe751
Levin LUN <chocolinax@gmail.com>
2021-03-11 09:32:21 +0800

    config auth0

--- routes/api.php
+++ routes/api.php
@@ -70,3 +70,14 @@ Route::post('/module/create', function (Request $request) {
 
     return $response;
 });
+
+
+// This endpoint does not need authentication.
+Route::get('/public', function (Request $request) {
+    return response()->json(["message" => "Hello from a public endpoint! You don't need to be authenticated to see this."]);
+});
+
+// These endpoints require a valid access token.
+Route::get('/private', function (Request $request) {
+    return response()->json(["message" => "Hello from a private endpoint! You need to have a valid access token to see this."]);
+})->middleware('jwt');
