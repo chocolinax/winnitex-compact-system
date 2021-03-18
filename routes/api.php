9279f3da3cf29e5fee430201be04e769c22fd641
Levin LUN <chocolinax@gmail.com>
2021-03-18 17:18:31 +0800

    used jwt instead of sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -19,42 +19,20 @@ use Illuminate\Support\Facades\Validator;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// This endpoint does not need authentication.
+// ...
 
-Route::post('/tokens/create', function (Request $request) {
-
-    $validator = Validator::make($request->all(), [
-        'device_id' => 'required',
-    ]);
-
-    if ($validator->fails()) {
-        $response = $validator->messages();
-    } else {
-
-        $user = Device::firstOrCreate([
-            'device_id' => $request->device_id
-        ]);
-
-        $token = $user->createToken($request->device_id);
-
-        $response = ['token' => $token->plainTextToken];
-    }
-
-    return $response;
-});
-
-Route::middleware('auth:sanctum')->get('/modules/get', function (Request $request) {
+// These endpoints require a valid access token.
+Route::middleware('jwt')->get('/modules/get', function (Request $request) {
     $modules = System::find(1)->modules;
     return $modules;
 });
 
-Route::middleware('auth:sanctum')->get('/pantry_items/get', function (Request $request) {
+Route::middleware('jwt')->get('/pantry_items/get', function (Request $request) {
     return PantryItem::all();
 });
 
-Route::middleware('auth:sanctum')->post('/pantry_items/add', function (Request $request) {
+Route::middleware('jwt')->post('/pantry_items/add', function (Request $request) {
 
     $validator = Validator::make($request->all(), [
         'code' => 'required|unique:pantry_items,code',
@@ -77,7 +55,7 @@ Route::middleware('auth:sanctum')->post('/pantry_items/add', function (Request $
     return $response;
 });
 
-Route::middleware('auth:sanctum')->post('/pantry_items/del', function (Request $request) {
+Route::middleware('jwt')->post('/pantry_items/del', function (Request $request) {
 
     $validator = Validator::make($request->all(), [
         'code' => 'required'
@@ -95,7 +73,7 @@ Route::middleware('auth:sanctum')->post('/pantry_items/del', function (Request $
 });
 
 
-
+// These endpoints require a valid access token with a "read:messages" scope.
 Route::post('/module/create', function (Request $request) {
 
     $validator = Validator::make($request->all(), [
@@ -115,21 +93,3 @@ Route::post('/module/create', function (Request $request) {
 
     return $response;
 });
-
-
-// This endpoint does not need authentication.
-Route::get('/public', function (Request $request) {
-    return response()->json(["message" => "Hello from a public endpoint! You don't need to be authenticated to see this."]);
-});
-
-// These endpoints require a valid access token.
-Route::get('/private', function (Request $request) {
-    return response()->json(["message" => "Hello from a private endpoint! You need to have a valid access token to see this."]);
-})->middleware('jwt');
-
-// These endpoints require a valid access token with a "read:messages" scope.
-Route::get('/private-scoped', function (Request $request) {
-    return response()->json([
-        "message" => "Hello from a private endpoint! You need to have a valid access token and a scope of read:messages to see this."
-    ]);
-})->middleware('check.scope:read:messages');
