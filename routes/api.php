92bd374301838d616abdef3eb48aea204073720d
Levin LUN <chocolinax@gmail.com>
2021-03-24 09:47:07 +0800

    no message

--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,7 @@ Route::middleware('jwt')->post('/modules/get', function (Request $request) {
         $modules = System::find(1)->modules()->get();
 
         $response = $modules->map(function ($module) use ($request) {
-            return $module->roles()->whereIn('module_allow_roles.name', $request->role)->get();
+            return $module->roles()->get();
         })->all();
     }
 
