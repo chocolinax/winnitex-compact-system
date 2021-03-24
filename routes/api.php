7e43a0fde38dd6fb531477193ea8c1a4ca83443e
Levin LUN <chocolinax@gmail.com>
2021-03-24 14:37:40 +0800

    no message

--- routes/api.php
+++ routes/api.php
@@ -37,7 +37,7 @@ Route::middleware('jwt')->post('/modules/get', function (Request $request) {
 
         $response = $modules->map(function ($module) use ($request) {
             $role_names = $module->roles()->pluck('name')->toArray();
-            // $result = array_intersect($request->roles, $role_names);
+            $result = array_intersect($request->roles, $role_names);
             return $module;
         });
     }
