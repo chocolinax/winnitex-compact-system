bb1d9a6e74c50a5b625002f28c2f96cc1ad922ee
Levin LUN <chocolinax@gmail.com>
2021-03-25 12:28:59 +0800

    no message

--- routes/api.php
+++ routes/api.php
@@ -43,7 +43,7 @@ Route::middleware('jwt')->post('/modules/get', function (Request $request) {
         // });
     }
 
-    return $modules->find(1)->roles()->select('name')->get()->pluck('name');
+    return $modules->find(1)->roles()->select('name')->pluck('name');
 });
 
 Route::middleware('jwt')->get('/pantry_items/get', function (Request $request) {
