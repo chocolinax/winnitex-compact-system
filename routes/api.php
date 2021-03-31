37a255709a5e3ce3a90fc1c4a8e3dfa4e92236de
Levin LUN <chocolinax@gmail.com>
2021-03-31 09:29:59 +0800

    no message

--- routes/api.php
+++ routes/api.php
@@ -51,7 +51,7 @@ Route::middleware('jwt')->post('/asset/add', function (Request $request) {
         'name' => 'required',
         'ext' => 'required',
         'loc' => 'required',
-        'assets' => 'required|json'
+        'assets' => 'required'
     ]);
 
     if ($validator->fails()) {
