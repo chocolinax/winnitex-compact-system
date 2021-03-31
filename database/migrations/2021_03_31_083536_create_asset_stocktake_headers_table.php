97260ae9c748fb056406b7bc6cb34a693a1de8d5
Levin LUN <chocolinax@gmail.com>
2021-03-31 18:12:38 +0800

    no message

--- database/migrations/2021_03_31_083536_create_asset_stocktake_headers_table.php
+++ database/migrations/2021_03_31_083536_create_asset_stocktake_headers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAssetStocktakeHeaderTable extends Migration
+class CreateAssetStocktakeHeadersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateAssetStocktakeHeaderTable extends Migration
      */
     public function up()
     {
-        Schema::create('asset_stocktake_header', function (Blueprint $table) {
+        Schema::create('asset_stocktake_headers', function (Blueprint $table) {
             $table->id();
             $table->string('team');
             $table->string('name');
@@ -31,6 +31,6 @@ class CreateAssetStocktakeHeaderTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('asset_stocktake_header');
+        Schema::dropIfExists('asset_stocktake_headers');
     }
 }
--- database/migrations/2021_03_31_084400_create_asset_stocktake_lines_table.php
+++ database/migrations/2021_03_31_084400_create_asset_stocktake_lines_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAssetStocktakeLineTable extends Migration
+class CreateAssetStocktakeLinesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateAssetStocktakeLineTable extends Migration
      */
     public function up()
     {
-        Schema::create('asset_stocktake_line', function (Blueprint $table) {
+        Schema::create('asset_stocktake_lines', function (Blueprint $table) {
             $table->id();
             $table->integer('asset_stocktake_header_id')->unsigned();
             $table->string('name');
@@ -32,6 +32,6 @@ class CreateAssetStocktakeLineTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('asset_stocktake_line');
+        Schema::dropIfExists('asset_stocktake_lines');
     }
 }
