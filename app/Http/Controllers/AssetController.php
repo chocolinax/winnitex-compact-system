14b7741900df53cdc5b705262f2d71d726217fd3
Levin LUN <chocolinax@gmail.com>
2021-05-12 15:35:42 +0800

    no message

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -11,7 +11,7 @@ class AssetController extends Controller
         switch ($groupBy) {
             case 'user':
                 $info = DB::table('record_lists')
-                    ->select('wtxusers.full_name_eng', DB::raw("string_agg(brands.brand, ', ') as brands"), DB::raw('count(*) as total'))
+                    ->select('wtxusers.id', 'wtxusers.full_name_eng', DB::raw("string_agg(brands.brand, ', ') as brands"), DB::raw('count(*) as total'))
                     ->join('wtxusers', 'wtxusers.id', '=', 'record_lists.wtxuser_id')
                     ->join('assets', 'assets.id', '=', 'record_lists.asset_id')
                     ->join('brands', 'brands.id', '=', 'assets.brand_id')
@@ -21,7 +21,7 @@ class AssetController extends Controller
 
             case 'dept':
                 $info = DB::table('record_lists')
-                    ->select('departments.department', DB::raw("string_agg(brands.brand, ', ') as brands"), DB::raw('count(*) as total'))
+                    ->select('departments.id', 'departments.department', DB::raw("string_agg(brands.brand, ', ') as brands"), DB::raw('count(*) as total'))
                     ->join('wtxusers', 'wtxusers.id', '=', 'record_lists.wtxuser_id')
                     ->join('departments', 'departments.id', '=', 'wtxusers.department_id')
                     ->join('assets', 'assets.id', '=', 'record_lists.asset_id')
@@ -32,7 +32,7 @@ class AssetController extends Controller
 
             case 'type':
                 $info = DB::table('record_lists')
-                    ->select('types.type', DB::raw("string_agg(departments.department, ', ') as departments"), DB::raw('count(*) as total'))
+                    ->select('types.id', 'types.type', DB::raw("string_agg(departments.department, ', ') as departments"), DB::raw('count(*) as total'))
                     ->join('wtxusers', 'wtxusers.id', '=', 'record_lists.wtxuser_id')
                     ->join('departments', 'departments.id', '=', 'wtxusers.department_id')
                     ->join('assets', 'assets.id', '=', 'record_lists.asset_id')
@@ -43,7 +43,7 @@ class AssetController extends Controller
 
             case 'brand':
                 $info = DB::table('record_lists')
-                    ->select('brands.brand', DB::raw("string_agg(assets.model_no, ', ') as model_no"), DB::raw('count(*) as total'))
+                    ->select('brands.id', 'brands.brand', DB::raw("string_agg(assets.model_no, ', ') as model_no"), DB::raw('count(*) as total'))
                     ->join('assets', 'assets.id', '=', 'record_lists.asset_id')
                     ->join('brands', 'brands.id', '=', 'assets.brand_id')
                     ->groupBy('brands.brand')
