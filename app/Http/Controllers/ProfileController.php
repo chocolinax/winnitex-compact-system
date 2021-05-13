d86dbc59eac3c541c6646a5c161d046edfb6dd2c
Levin LUN <chocolinax@gmail.com>
2021-05-13 11:26:27 +0800

    no message

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -22,21 +22,25 @@ class ProfileController extends Controller
 
             case 'type':
                 $info = DB::table('record_lists')
-                    ->select('types.type', 'departments.department', DB::raw('count(*) as total'))
+                    ->select('wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
                     ->join('wtxusers', 'wtxusers.id', '=', 'record_lists.wtxuser_id')
                     ->join('departments', 'departments.id', '=', 'wtxusers.department_id')
-                    ->join('assets', 'assets.id', '=', 'record_lists.asset_id')
+                    ->join('locations', 'locations.id', '=', 'record_lists.location_id')
                     ->join('types', 'types.id', '=', 'assets.type_id')
-                    ->groupBy('types.type', 'departments.department')
+                    ->where('types.id', '=', $id)
+                    ->groupBy('wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
                     ->get();
                 break;
 
             case 'brand':
                 $info = DB::table('record_lists')
-                    ->select('brands.brand', 'assets.model_no', DB::raw('count(*) as total'))
-                    ->join('assets', 'assets.id', '=', 'record_lists.asset_id')
+                    ->select('wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
+                    ->join('wtxusers', 'wtxusers.id', '=', 'record_lists.wtxuser_id')
+                    ->join('departments', 'departments.id', '=', 'wtxusers.department_id')
+                    ->join('locations', 'locations.id', '=', 'record_lists.location_id')
                     ->join('brands', 'brands.id', '=', 'assets.brand_id')
-                    ->groupBy('brands.brand', 'assets.model_no')
+                    ->where('brands.id', '=', $id)
+                    ->groupBy('wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
                     ->get();
                 break;
 
