69466bfbc8df9ea0ddb2ac32287859615121cf58
Levin LUN <chocolinax@gmail.com>
2021-05-13 17:03:50 +0800

    no message

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -29,12 +29,21 @@ class AssetController extends Controller
                 break;
 
             case 'dept':
+                $subQuery = DB::table('record_lists')
+                    ->select('wtxuser_id', 'assets.brand_id', DB::raw('count(*) as ttlbybrand'))
+                    ->join('assets', 'assets.id', '=', 'record_lists.asset_id')
+                    ->groupBy('wtxuser_id', 'assets.brand_id');
+
                 $info = DB::table('record_lists')
-                    ->select('departments.id', 'departments.department', DB::raw("string_agg(brands.brand, ', ') as brands"), DB::raw('count(*) as total'))
+                    ->select('departments.id', 'departments.department', DB::raw("string_agg(concat(brands.brand,': ', sub.ttlbybrand), ', ') as ttlbybrand"))
                     ->join('wtxusers', 'wtxusers.id', '=', 'record_lists.wtxuser_id')
                     ->join('departments', 'departments.id', '=', 'wtxusers.department_id')
                     ->join('assets', 'assets.id', '=', 'record_lists.asset_id')
                     ->join('brands', 'brands.id', '=', 'assets.brand_id')
+                    ->joinSub($subQuery, 'sub', function ($join) {
+                        $join->on('wtxusers.id', '=', 'sub.wtxuser_id')
+                            ->on('brands.id', '=', 'sub.brand_id');
+                    })
                     ->groupBy('departments.id', 'departments.department')
                     ->get();
                 break;
