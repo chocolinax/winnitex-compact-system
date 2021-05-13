d1f8385bb4a904087c66d4b2862cf56950733150
Levin LUN <chocolinax@gmail.com>
2021-05-13 16:20:24 +0800

    no message

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -15,16 +15,16 @@ class AssetController extends Controller
                     ->join('assets', 'assets.id', '=', 'record_lists.asset_id')
                     ->groupBy('wtxuser_id', 'assets.brand_id')->get();
 
-                $info = DB::table('record_lists')
-                    ->select('wtxusers.id', 'wtxusers.full_name_eng', DB::raw("string_agg(concat(brands.brand,': ', sub.ttlbybrand), ', ') as ttlbybrand"))
-                    ->join('wtxusers', 'wtxusers.id', '=', 'record_lists.wtxuser_id')
-                    ->join('assets', 'assets.id', '=', 'record_lists.asset_id')
-                    ->join('brands', 'brands.id', '=', 'assets.brand_id')
-                    ->joinSub($subQuery, 'sub', function ($join) {
-                        $join->on('wtxusers.id', '=', 'record_lists.wtxuser_id')
-                            ->on('brands.id', '=', 'assets.brand_id');
-                    })->groupBy('wtxusers.id', 'wtxusers.full_name_eng')
-                    ->get();
+                // $info = DB::table('record_lists')
+                //     ->select('wtxusers.id', 'wtxusers.full_name_eng', DB::raw("string_agg(concat(brands.brand,': ', sub.ttlbybrand), ', ') as ttlbybrand"))
+                //     ->join('wtxusers', 'wtxusers.id', '=', 'record_lists.wtxuser_id')
+                //     ->join('assets', 'assets.id', '=', 'record_lists.asset_id')
+                //     ->join('brands', 'brands.id', '=', 'assets.brand_id')
+                //     ->joinSub($subQuery, 'sub', function ($join) {
+                //         $join->on('wtxusers.id', '=', 'record_lists.wtxuser_id')
+                //             ->on('brands.id', '=', 'assets.brand_id');
+                //     })->groupBy('wtxusers.id', 'wtxusers.full_name_eng')
+                //     ->get();
                 break;
 
             case 'dept':
