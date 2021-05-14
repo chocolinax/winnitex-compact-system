d3055afedfb53737f790462648c8cf7ae9f12404
Levin LUN <chocolinax@gmail.com>
2021-05-14 15:30:40 +0800

    no message

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,38 +11,38 @@ class ProfileController extends Controller
         switch ($groupBy) {
             case 'dept':
                 $info = DB::table('record_lists')
-                    ->select('wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
+                    ->select('wtxusers.id', 'wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
                     ->join('wtxusers', 'wtxusers.id', '=', 'record_lists.wtxuser_id')
                     ->join('departments', 'departments.id', '=', 'wtxusers.department_id')
                     ->join('locations', 'locations.id', '=', 'record_lists.location_id')
                     ->where('departments.id', '=', $id)
-                    ->groupBy('wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
+                    ->groupBy('wtxusers.id', 'wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
                     ->get();
                 break;
 
             case 'type':
                 $info = DB::table('record_lists')
-                    ->select('wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
+                    ->select('wtxusers.id', 'wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
                     ->join('wtxusers', 'wtxusers.id', '=', 'record_lists.wtxuser_id')
                     ->join('departments', 'departments.id', '=', 'wtxusers.department_id')
                     ->join('locations', 'locations.id', '=', 'record_lists.location_id')
                     ->join('assets', 'assets.id', '=', 'record_lists.asset_id')
                     ->join('types', 'types.id', '=', 'assets.type_id')
                     ->where('types.id', '=', $id)
-                    ->groupBy('wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
+                    ->groupBy('wtxusers.id', 'wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
                     ->get();
                 break;
 
             case 'brand':
                 $info = DB::table('record_lists')
-                    ->select('wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
+                    ->select('wtxusers.id', 'wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
                     ->join('wtxusers', 'wtxusers.id', '=', 'record_lists.wtxuser_id')
                     ->join('departments', 'departments.id', '=', 'wtxusers.department_id')
                     ->join('locations', 'locations.id', '=', 'record_lists.location_id')
                     ->join('assets', 'assets.id', '=', 'record_lists.asset_id')
                     ->join('brands', 'brands.id', '=', 'assets.brand_id')
                     ->where('brands.id', '=', $id)
-                    ->groupBy('wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
+                    ->groupBy('wtxusers.id', 'wtxusers.full_name_eng', 'wtxusers.ext', 'departments.department', 'departments.team', 'locations.location', 'stocktake_date')
                     ->get();
                 break;
 
