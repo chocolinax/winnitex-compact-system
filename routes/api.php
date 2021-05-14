da2fa46899016078f207a8e8e6eb0cfd0af475a5
Levin LUN <chocolinax@gmail.com>
2021-05-14 17:41:57 +0800

    no message

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,7 @@ Route::get('/user_profile/get/{id}', function ($id) {
     $info = DB::table('record_lists')
         ->select('record_lists.wtxuser_id', 'brands.brand', 'types.type', 'assets.model_no', 'record_lists.stocktake_date')
         ->join('assets', 'assets.id', '=', 'record_lists.asset_id')
-        ->join('brands', 'brands.id', '=', 'record_lists.brand_id')
+        ->join('brands', 'brands.id', '=', 'assets.brand_id')
         ->join('types', 'types.id', '=', 'record_lists.type_id')
         ->where('record_lists.wtxuser_id', '=', $id)
         ->groupBy('record_lists.wtxuser_id', 'brands.brand', 'types.type', 'assets.model_no', 'record_lists.stocktake_date')
